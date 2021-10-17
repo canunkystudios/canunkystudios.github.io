502e7ab53f0b65feb9c67581f83104520fedf9f0
grimmick <mikeyoakes@gmail.com>
2021-10-17 13:15:07 -0600

    id

--- reaperGame/Obstacles.js
+++ reaperGame/Obstacles.js
@@ -48,7 +48,7 @@ class Obstacles{
 	}	
 
     loadOrb(){
-    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}../../blob/main/assets/reaper/`);
+    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}reaper/`);
         
 		// Load a glTF resource
 		loader.load(
